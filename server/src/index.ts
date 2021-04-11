ea58706af104194d8a4a8cc58c072f70dc92073f
Justin Reardon <justin.reardon@gmail.com>
2021-04-11 09:22:49 -0400

    server tweaks to work on heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import proxy from 'express-http-proxy';
 
 const app = express();
 
 //app.use(function (req, res, next) { setTimeout(next, 1000) });
-app.use('/api/weather/', proxy('https://dd.weather.gc.ca/'));
+app.use('/api/weather/', cors(), proxy('https://dd.weather.gc.ca/'));
 app.listen(process.env.PORT || 8080);
