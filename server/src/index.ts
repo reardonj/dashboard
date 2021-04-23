09c95f574fe5577a513611b93c439e347fd3010e
Justin Reardon <justin.reardon@gmail.com>
2021-04-22 21:07:23 -0400

    restrict cors to my own domain

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,10 @@ import proxy from 'express-http-proxy';
 
 const app = express();
 
+var corsOptions = {
+  origin: 'https://www.jmreardon.com',
+}
+
 //app.use(function (req, res, next) { setTimeout(next, 1000) });
-app.use('/api/weather/', cors(), proxy('https://dd.weather.gc.ca/'));
+app.use('/api/weather/', cors(corsOptions), proxy('https://dd.weather.gc.ca/'));
 app.listen(process.env.PORT || 8080);
