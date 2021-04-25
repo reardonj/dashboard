72341de56f9667e57dedf3ea380dc85e2fc4d9db
Justin Reardon <justin.reardon@gmail.com>
2021-04-24 22:38:08 -0400

    use env to use local server in dev

--- front-end/src/features/weather/WeatherSlice.ts
+++ front-end/src/features/weather/WeatherSlice.ts
@@ -185,7 +185,10 @@ function parseWeather(xml: Document): WeatherReport {
 
 // Thunk functions
 export const fetchWeather = createAsyncThunk('weather/fetchWeather', async () => {
-  const response = await axios('https://dashboard-proxy.jmreardon.com/api/weather/citypage_weather/xml/ON/s0000430_e.xml');
+  const response = await axios(
+    process.env.NODE_ENV === 'production'
+      ? 'https://dashboard-proxy.jmreardon.com/api/weather/citypage_weather/xml/ON/s0000430_e.xml'
+      : '/api/weather/citypage_weather/xml/ON/s0000430_e.xml');
   const xml = new DOMParser().parseFromString(response.data, "text/xml");
   try {
     return parseWeather(xml);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import cors from 'cors';
+import cors, { CorsOptions, CorsRequest } from 'cors';
 import proxy from 'express-http-proxy';
 
 const app = express();
 
-var corsOptions = {
-  origin: 'https://www.jmreardon.com',
+var corsOptions: CorsOptions = {
+  origin: process.env.PORT ? 'https://www.jmreardon.com' : undefined,
 }
 
 //app.use(function (req, res, next) { setTimeout(next, 1000) });
